fb20dab953e67ef504aedc83431040d95ae22e3a
Oren Magid <oren.michael.magid@gmail.com>
2021-04-16 19:56:08 -0400

    fixed backslash on baseUrl

--- src/App.js
+++ src/App.js
@@ -2,46 +2,43 @@ import Status from "./components/Status"
 
 function App() {
   return (
-    <div>
-      <section>
-        <header>
-          <h1 style={{ textAlign: "center" }}>
-            CVS DC COVID Vaccine Availability Screamer
-          </h1>
-        </header>
-        <main>
-          <p>
-            This application will scream at you if there are vaccine
-            appointments available at a CVS in DC. This is likely to happen in
-            the middle of the night. I suggest leaving this open in your
-            browser, making sure your computer's volume is high enough to wake
-            you up (test it below), and{" "}
-            <a href="https://wedflow.zendesk.com/hc/en-us/articles/360037926892-How-to-prevent-your-computer-from-going-to-sleep-during-large-uploads-">
-              making sure your computer won't go to sleep when you do.
-            </a>
-          </p>
-          <p>
-            When there's availability, go{" "}
-            <a
-              href="https://www.cvs.com/immunizations/covid-19-vaccine"
-              target="_blank"
-            >
-              here
-            </a>{" "}
-            to sign up.
-          </p>
-          <p>
-            Be warned that CVS's website is very buggy, and you may have to try
-            again and again after encountering the following error:{" "}
-            <strong>
-              "We can't continue due to a technical glitch. Please try again
-              later."
-            </strong>
-          </p>
-          <Status />
-        </main>
-      </section>
-    </div>
+    <section>
+      <header style={{ textAlign: "center" }}>
+        <h1>CVS DC COVID Vaccine Availability Screamer</h1>
+        Made by <a href="https://github.com/orenmagid">Oren Magid</a>
+      </header>
+      <main>
+        <p>
+          This application will scream at you if there are vaccine appointments
+          available at a CVS in DC. This is likely to happen in the middle of
+          the night. I suggest leaving this open in your browser, making sure
+          your computer's volume is high enough to wake you up (test it below),
+          and{" "}
+          <a href="https://wedflow.zendesk.com/hc/en-us/articles/360037926892-How-to-prevent-your-computer-from-going-to-sleep-during-large-uploads-">
+            making sure your computer won't go to sleep when you do.
+          </a>
+        </p>
+        <p>
+          When there's availability, go{" "}
+          <a
+            href="https://www.cvs.com/immunizations/covid-19-vaccine"
+            target="_blank"
+          >
+            here
+          </a>{" "}
+          to sign up.
+        </p>
+        <p>
+          Be warned that CVS's website is very buggy, and you may have to try
+          again and again after encountering the following error:{" "}
+          <strong>
+            "We can't continue due to a technical glitch. Please try again
+            later."
+          </strong>
+        </p>
+        <Status />
+      </main>
+    </section>
   )
 }
 
--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import { baseUrl } from "../constants"
 
 export const getStatus = async () => {
-  const response = await fetch(baseUrl + `cvs-vaccine-status-updater/DC`)
+  const response = await fetch(baseUrl + `/cvs-vaccine-status-updater/DC`)
 
   if (!response.ok) {
     throw new Error("Something went wrong!")
